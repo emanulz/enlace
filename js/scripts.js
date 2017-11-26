f9d9663ada7e325b475ddb44059edb9ca9662d5e
Emanuel Zuniga Infante <emanuelziga@gmail.com>
2017-11-25 19:09:15 -0600

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+(function ($, root, undefined) {
+
+	$(function () {
+
+		'use strict';
+
+		$('.icon').on('click' ,function(){
+			$('#sideMenu').toggleClass('visible')
+		})
+
+
+	});
+
+})(jQuery, this);
---
+++ webpack.config.js
@@ -0,0 +1,160 @@
+const debug = process.env.NODE_ENV !== 'production'
+const webpack = require('webpack')
+const path = require('path')
+// const CompressionPlugin = require('compression-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const LiveReloadPlugin = require('webpack-livereload-plugin')
+
+const config = {
+  // TODO: Add common Configuration
+  module: {}
+}
+
+// const jsConfig = Object.assign({}, config, {
+//   context: __dirname,
+//   devtool: debug ? 'inline-sourcemap' : '',
+//   name: 'js',
+//   entry: {
+//     // landing: './frontend/landing/app.js',
+//     admin: './frontend/admin/app.js',
+//     inventories: './frontend/inventories/app.js',
+//     landing: './frontend/landing/app.js',
+//     pos: './frontend/pos/app.js',
+//     reports: './frontend/reports/app.js',
+//     sales: './frontend/sales/app.js'
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js|jsx)$/,
+//         exclude: /(node_modules|bower_components)/,
+//         loader: 'babel-loader'
+//       }
+//     ]
+//   },
+//   output: {
+//     path: path.join(__dirname, '/public'),
+//     filename: './js/[name].js'
+//   },
+//
+//   plugins: debug
+//     ? [
+//       new LiveReloadPlugin()
+//     ]
+//     : [
+//       new webpack.DefinePlugin({
+//         'process.env': {
+//           'NODE_ENV': JSON.stringify('production')
+//         }
+//       }),
+//       // new JavaScriptObfuscator({
+//       //   rotateUnicodeArray: true
+//       // }, []),
+//       new webpack.optimize.UglifyJsPlugin({
+//         comments: false,
+//         compress: {
+//           // remove warnings
+//           warnings: false,
+//
+//           // Drop console statements
+//           drop_console: true
+//         }
+//       }),
+//       new webpack.optimize.OccurrenceOrderPlugin()
+//     ]
+//
+// })
+
+const stylesConfig = Object.assign({}, config, {
+  context: __dirname,
+  devtool: debug ? 'inline-sourcemap' : '',
+  name: 'styles',
+  entry: {
+    // landing: './frontend/landing/main.sass',
+    main: './css/_all.sass'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sass|css)$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      }
+    ]
+  },
+  output: {
+    path: path.join(__dirname, ''),
+    filename: './css/[name].css'
+  },
+
+  plugins: debug
+    ? [
+      new ExtractTextPlugin({filename: './css/[name].css', allChunks: true}),
+      new LiveReloadPlugin()
+    ]
+    : [
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
+      new ExtractTextPlugin({filename: './[name].css', allChunks: true}),
+      new webpack.optimize.UglifyJsPlugin(),
+      new webpack.optimize.OccurrenceOrderPlugin()
+    ]
+
+})
+
+// module.exports = [
+//   jsConfig, stylesConfig
+// ]
+
+module.exports = [
+  stylesConfig
+]
+
+// module.exports = {
+//   context: __dirname,
+//   devtool: debug ? "inline-sourcemap" : '',
+//   entry: {
+//     landing:"./frontend/landing/app.js",
+//     landingStyles:"./frontend/landing/main.sass",
+//     pos:"./frontend/pos/app.js",
+//     posStyles:"./frontend/pos/styles/main.sass",
+//     admin:"./frontend/admin/app.js",
+//     adminStyles:"./frontend/admin/styles/styles.sass"
+//   },
+//
+//   module:{
+//     rules: [
+//           {
+//               test: /\.(js|jsx)$/,
+//               exclude: /(node_modules|bower_components)/,
+//               loader: 'babel-loader',
+//           },
+//           {
+//               test:/\.(sass|css)$/,
+//               loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+//           }
+//         ],
+//     },
+//
+//   output: {
+//     path: __dirname+ "/public",
+//     filename: "./js/[name].js"
+//   },
+//
+//   plugins: debug ?
+//                 [new ExtractTextPlugin({filename: './css/[name].css', allChunks: true}),
+//                 new LiveReloadPlugin()]
+//                 :
+//                 [
+//                 new webpack.DefinePlugin({
+//                   'process.env': {
+//                     'NODE_ENV': JSON.stringify('production')
+//                   }
+//                 }),
+//                 new ExtractTextPlugin({filename:"./css/[name].css", allChunks: true}),
+//                 new webpack.optimize.UglifyJsPlugin()
+//               ],
+//
+// };
