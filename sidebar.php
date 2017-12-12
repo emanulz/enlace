77e4f55548e47c67a325adfdaf03f4fd79746e33
Emanuel Zuniga Infante <emanuelziga@gmail.com>
2017-12-11 19:08:39 -0600

    0.0.21 Minor Changes
    Minor Changes

--- sidebar.php
+++ sidebar.php
@@ -5,17 +5,25 @@
 	<?php get_template_part('sideBannerBig'); ?>
 	<!-- SIDE BANNER BIG-->
 
+	<hr>
+
 	<div class="sidebar-widget">
 		<?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('widget-area-1')) ?>
 	</div>
+
+	<hr>
 
 	<!-- SIDE BANNER SMALL 1 -->
 	<?php get_template_part('sideBannerSmall1'); ?>
 	<!-- SIDE BANNER SMALL 1-->
 
+	<hr>
+
 	<div class="sidebar-widget">
 		<?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('widget-area-2')) ?>
 	</div>
+
+	<hr>
 
 	<!-- SIDE BANNER SMALL 2 -->
 	<?php get_template_part('sideBannerSmall2'); ?>
