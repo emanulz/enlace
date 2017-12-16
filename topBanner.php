2ee3592cd2be2f5ce0b7176a004a850764ceb44e
Emanuel Zuniga Infante <emanuelziga@gmail.com>
2017-12-15 19:39:27 -0600

    0.1.3 Removed Top Banner
    Removed Top Banner

--- topBanner.php
+++ topBanner.php
@@ -1,12 +1,12 @@
 <section class="homeBanner">
 
-    <div id="topBanner">
+    <!-- <div id="topBanner">
       <a href="http://www.frenteamplio.org/" target="_blank">
         <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/top1.png" alt="">
       </a>
       <a href="http://www.sergio2018.com/" target="_blank">
         <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/top2.png" alt="">
       </a>
-    </div>
+    </div> -->
 
 </section>
