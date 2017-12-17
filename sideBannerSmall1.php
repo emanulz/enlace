fb1feed8b924b2f4fe4eea17b53e73ea2fb3208a
Emanuel Zuniga Infante <emanuelziga@gmail.com>
2017-12-17 08:23:42 -0600

    0.1.5 Added Coopeagri Banner
    Added Coopeagri Banner

--- sideBannerSmall1.php
+++ sideBannerSmall1.php
@@ -1,6 +1,9 @@
 <section class="sideBannerSmall1">
 
     <div id="sideBannerSmall1">
+      <a href="https://www.facebook.com/supermercadoscoopeagri/" target="_blank">
+        <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/coopeagri.jpg" alt="">
+      </a>
       <a href="https://www.facebook.com/UNCOOSUR-144860892251994/" target="_blank">
         <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/uncoosur.jpg" alt="">
       </a>
