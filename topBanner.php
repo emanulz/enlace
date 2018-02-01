746fce3a9a17afe9ed6c906a318de56c374a5ebb
Emanuel <emanuzuniga@gmail.com>
2018-01-31 22:26:58 -0600

    0.1.12 Removed political banners
    Removed political banners

--- topBanner.php
+++ topBanner.php
@@ -4,21 +4,24 @@
       <a href="https://coopealianza.fi.cr/" target="_blank">
         <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/coopealianza.jpg" alt="">
       </a>
-      <a href="http://www.frenteamplio.org/" target="_blank">
-        <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/frenteAmplio1.jpg" alt="">
-      </a>
-      <a href="http://www.frenteamplio.org/" target="_blank">
-        <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/frenteAmplio2.jpg" alt="">
-      </a>
-      <a href="http://www.sergio2018.com/" target="_blank">
-        <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/png.jpg" alt="">
-      </a>
       <a href="http://www.infocoop.go.cr/" target="_blank">
         <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/infocoop.jpg" alt="">
       </a>
-      <a href="http://www.plncr.org/" target="_blank">
-        <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/pln.jpg" alt="">
-      </a>
+
     </div>
 
 </section>
+
+
+<!-- <a href="http://www.frenteamplio.org/" target="_blank">
+  <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/frenteAmplio1.jpg" alt="">
+</a>
+<a href="http://www.frenteamplio.org/" target="_blank">
+  <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/frenteAmplio2.jpg" alt="">
+</a>
+<a href="http://www.sergio2018.com/" target="_blank">
+  <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/png.jpg" alt="">
+</a> -->
+<!-- <a href="http://www.plncr.org/" target="_blank">
+  <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/pln.jpg" alt="">
+</a> -->
