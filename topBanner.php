306346479deca93116f39a35165d93ae333f1989
Emanuel <emanuzuniga@gmail.com>
2018-03-21 10:30:59 -0600

    0.2.7 Added 2 Banners
    Added 2 Banners

--- topBanner.php
+++ topBanner.php
@@ -4,6 +4,15 @@
       <a href="https://www.bncr.fi.cr/BNCR/BancaHipotecaria/PrestVivienda.aspx" target="_blank">
         <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/bn.GIF" alt="">
       </a>
+      <a href="https://carlos.cr/" target="_blank">
+        <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/carlosAlvarado.jpg" alt="">
+      </a>
+      <a>
+        <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/feriaCooperativa.jpg" alt="">
+      </a>
+      <a href="https://www.bncr.fi.cr/BNCR/BancaHipotecaria/PrestVivienda.aspx" target="_blank">
+        <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/bn.GIF" alt="">
+      </a>
       <a href="http://www.cadesurcr.com/" target="_blank">
         <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/cadesur.jpg" alt="">
       </a>
