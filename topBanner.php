25dc4aaba1c805b0d89b15a0202a10d9dc87a643
Emanuel <emanuzuniga@gmail.com>
2018-03-28 10:06:19 -0600

    0.2.8 Banners removed
    Banners removed

--- topBanner.php
+++ topBanner.php
@@ -4,14 +4,8 @@
       <a href="https://www.bncr.fi.cr/BNCR/BancaHipotecaria/PrestVivienda.aspx" target="_blank">
         <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/bn.GIF" alt="">
       </a>
-      <a href="https://carlos.cr/" target="_blank">
-        <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/carlosAlvarado.jpg" alt="">
-      </a>
       <a>
         <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/feriaCooperativa.jpg" alt="">
-      </a>
-      <a href="https://www.bncr.fi.cr/BNCR/BancaHipotecaria/PrestVivienda.aspx" target="_blank">
-        <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/bn.GIF" alt="">
       </a>
       <a href="http://www.cadesurcr.com/" target="_blank">
         <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/cadesur.jpg" alt="">
@@ -26,17 +20,3 @@
     </div>
 
 </section>
-
-
-<!-- <a href="http://www.frenteamplio.org/" target="_blank">
-  <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/frenteAmplio1.jpg" alt="">
-</a>
-<a href="http://www.frenteamplio.org/" target="_blank">
-  <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/frenteAmplio2.jpg" alt="">
-</a>
-<a href="http://www.sergio2018.com/" target="_blank">
-  <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/png.jpg" alt="">
-</a> -->
-<!-- <a href="http://www.plncr.org/" target="_blank">
-  <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/pln.jpg" alt="">
-</a> -->
