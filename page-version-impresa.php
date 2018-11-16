b0cde4ac53b1a50ae695139224c9af48e8ad568a
Emanuel Zuniga <emanuelziga@gmail.com>
2018-11-15 18:54:45 -0600

    Added November printed version

--- page-version-impresa.php
+++ page-version-impresa.php
@@ -10,6 +10,10 @@
 			<div class="homeMain">
 				<div data-configid="12157890/65284082" style="width:100%; height:600px;" class="issuuembed"></div>
 				<script type="text/javascript" src="//e.issuu.com/embed.js" async="true"></script>
+			<!-- OCTUBRE
+			<div data-configid="12157890/65284082" style="width:100%; height:600px;" class="issuuembed"></div>
+			<script type="text/javascript" src="//e.issuu.com/embed.js" async="true"></script>
+			-->
 			<!-- SETIEMBRE
 			<div data-configid="12157890/64627332" style="width:100%; height:600px;" class="issuuembed"></div>
 			<script type="text/javascript" src="//e.issuu.com/embed.js" async="true"></script>
