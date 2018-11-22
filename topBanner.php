0178db144cf2409461343b4f4360af7311f326ea
Emanuel Zuniga <emanuelziga@gmail.com>
2018-11-22 16:16:01 -0600

    Added Cosevi Banner
    Added Cosevi Banner

--- topBanner.php
+++ topBanner.php
@@ -4,6 +4,9 @@
       <a href="https://coopealianza.fi.cr/" target="_blank">
         <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/coopealianza2.png" alt="">
       </a>
+      <a href="#">
+        <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/cosevi.jpeg" alt="">
+      </a>
       <a href="https://vamosaturistear.com/" target="_blank">
         <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/ICT.png" alt="">
       </a>
