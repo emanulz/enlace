167f1743566902adb5c908045f47bd38dfc3d772
Emanuel Zuniga <emanuelziga@gmail.com>
2018-12-14 07:10:07 -0600

    changed printed version for december
    changed printed version for december

--- page-version-impresa.php
+++ page-version-impresa.php
@@ -8,36 +8,40 @@
 		<section class="homeContainer">
 			
 			<div class="homeMain">
+				<div data-configid="12157890/66402404" style="width:100%; height:600px;" class="issuuembed"></div>
+				<script type="text/javascript" src="//e.issuu.com/embed.js" async="true"></script>
+				<!-- OCTUBRE
 				<div data-configid="12157890/65953214" style="width:100%; height:600px;" class="issuuembed"></div>
 				<script type="text/javascript" src="//e.issuu.com/embed.js" async="true"></script>
-			<!-- OCTUBRE
-			<div data-configid="12157890/65284082" style="width:100%; height:600px;" class="issuuembed"></div>
-			<script type="text/javascript" src="//e.issuu.com/embed.js" async="true"></script>
-			-->
-			<!-- SETIEMBRE
-			<div data-configid="12157890/64627332" style="width:100%; height:600px;" class="issuuembed"></div>
-			<script type="text/javascript" src="//e.issuu.com/embed.js" async="true"></script>
-			-->
-			<!-- AGOSTO
-			<div data-configid="12157890/64285276" style="width:100%; height:600px;" class="issuuembed"></div>
-			<script type="text/javascript" src="//e.issuu.com/embed.js" async="true"></script>
-			-->
-			<!-- JUNIO
-			<div data-configid="12157890/63447105" style="width:100%; height:600px;" class="issuuembed"></div>
-			<script type="text/javascript" src="//e.issuu.com/embed.js" async="true"></script>
-			-->
-			<!-- JUNIO
-			<div data-configid="12157890/62551738" style="width:100%; height:600px;" class="issuuembed"></div>
-			<script type="text/javascript" src="//e.issuu.com/embed.js" async="true"></script>
-			-->
-			<!-- MAYO
-			<div data-configid="12157890/61407688" style="width:100%; height:600px;" class="issuuembed"></div>
-			<script type="text/javascript" src="//e.issuu.com/embed.js" async="true"></script>
-			-->
-			<!-- ABRIL
-			<div data-configid="12157890/60296339" style="width:100%; height:600px;" class="issuuembed"></div>
-			<script type="text/javascript" src="//e.issuu.com/embed.js" async="true"></script>
-			-->
+				-->
+				<!-- OCTUBRE
+				<div data-configid="12157890/65284082" style="width:100%; height:600px;" class="issuuembed"></div>
+				<script type="text/javascript" src="//e.issuu.com/embed.js" async="true"></script>
+				-->
+				<!-- SETIEMBRE
+				<div data-configid="12157890/64627332" style="width:100%; height:600px;" class="issuuembed"></div>
+				<script type="text/javascript" src="//e.issuu.com/embed.js" async="true"></script>
+				-->
+				<!-- AGOSTO
+				<div data-configid="12157890/64285276" style="width:100%; height:600px;" class="issuuembed"></div>
+				<script type="text/javascript" src="//e.issuu.com/embed.js" async="true"></script>
+				-->
+				<!-- JUNIO
+				<div data-configid="12157890/63447105" style="width:100%; height:600px;" class="issuuembed"></div>
+				<script type="text/javascript" src="//e.issuu.com/embed.js" async="true"></script>
+				-->
+				<!-- JUNIO
+				<div data-configid="12157890/62551738" style="width:100%; height:600px;" class="issuuembed"></div>
+				<script type="text/javascript" src="//e.issuu.com/embed.js" async="true"></script>
+				-->
+				<!-- MAYO
+				<div data-configid="12157890/61407688" style="width:100%; height:600px;" class="issuuembed"></div>
+				<script type="text/javascript" src="//e.issuu.com/embed.js" async="true"></script>
+				-->
+				<!-- ABRIL
+				<div data-configid="12157890/60296339" style="width:100%; height:600px;" class="issuuembed"></div>
+				<script type="text/javascript" src="//e.issuu.com/embed.js" async="true"></script>
+				-->
 
 			</div>
 			<div class="homeSideBar">
