09d7ac5bb3d13dadba8aedaba22fe57f38b84230
Emanuel Zuniga <emanuelziga@gmail.com>
2019-05-27 07:30:13 -0600

    Added may printed version
    Added may printed version

--- page-version-impresa.php
+++ page-version-impresa.php
@@ -8,8 +8,12 @@
 		<section class="homeContainer">
 			
 			<div class="homeMain">
+				<div data-configid="12157890/70182140" style="width:100%; height:600px;" class="issuuembed"></div>
+				<script type="text/javascript" src="//e.issuu.com/embed.js" async="true"></script>
+				<!-- MARZO
 				<div data-configid="12157890/68668732" style="width:100%; height:600px;" class="issuuembed"></div>
 				<script type="text/javascript" src="//e.issuu.com/embed.js" async="true"></script>
+				-->
 				<!-- FEBRERO
 				<div data-configid="12157890/67918344" style="width:100%; height:600px;" class="issuuembed"></div>
 				<script type="text/javascript" src="//e.issuu.com/embed.js" async="true"></script>
