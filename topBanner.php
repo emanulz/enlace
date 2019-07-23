3830b92a7d552f0fd8f3b17e0b66410e3eff38da
emanulz <emanuelziga@gmail.com>
2019-07-22 18:26:00 -0600

    Deleted banner

--- topBanner.php
+++ topBanner.php
@@ -1,19 +1,15 @@
 <section class="homeBanner">
 
     <div id="topBanner">
+      <a href="http://www.infocoop.go.cr/" target="_blank">
+        <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/infocoop.jpg" alt="">
+      </a>
       <a href=# target="_blank">
         <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/mujeres.png" alt="">
-      </a>
-      <a href="https://coopealianza.fi.cr/" target="_blank">
-        <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/coopealianza2.png" alt="">
       </a>
       <a href="#" target="_blank">
         <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/aya.jpg" alt="">
       </a>
-      <a href="http://www.infocoop.go.cr/" target="_blank">
-        <img src="<?php echo get_bloginfo('template_url') ?>/img/adds/infocoop.jpg" alt="">
-      </a>
-
     </div>
 
 </section>
